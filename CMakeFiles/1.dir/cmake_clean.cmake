fab3aaa98fba7d1d1b1bf77ce6a08c96ac92787f
linuxgood <linuxgood@itcast.(none)>
2017-12-10 23:16:15 +0800

    achievement in linux

---
+++ CMakeFiles/1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/1.dir/1.cpp.o"
+  "1.pdb"
+  "1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/2.dir/2.cpp.o"
+  "2.pdb"
+  "2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
